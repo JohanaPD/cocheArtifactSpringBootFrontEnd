f403689278a27a3b725e9f6b2dd07c83c4e1768c
JohanaPD <118165435+JohanaPD@users.noreply.github.com>
2024-12-04 11:49:16 +0100

    04/12/2024

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { PruebaRoutingModule } from './prueba-routing.module';
+import { FeatureRoutingModule } from './feature-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    PruebaRoutingModule
+    FeatureRoutingModule
   ]
 })
-export class PruebaModule { }
+export class FeatureModule { }
