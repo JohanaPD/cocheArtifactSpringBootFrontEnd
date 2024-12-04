b101f7532a1351375940273bbf4c8a1f754d94b1
JohanaPD <118165435+JohanaPD@users.noreply.github.com>
2024-12-04 11:23:03 +0100

    04/12/2024
    .

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Palabras nuevas en esto');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Cambia titulo en ts');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Palabras nuevas en esto';
+  title = 'Cambia titulo en ts';
 }
