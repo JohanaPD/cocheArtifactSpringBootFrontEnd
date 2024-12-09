7b4c5dfb0216eab149139b43ce2409eee19bc3a6
JohanaPD <118165435+JohanaPD@users.noreply.github.com>
2024-12-09 11:43:31 +0100

    09/12/2024

--- src/app/feature/auth/auth.module.ts
+++ src/app/feature/auth/auth.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { AuthRoutingModule } from './auth-routing.module';
+import {AuthRoutingModule} from './auth-routing.module';
 import {LoginComponent} from "./pages/login/login.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
-  declarations: [
-
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     AuthRoutingModule,
     LoginComponent,
+    ReactiveFormsModule
   ]
 })
-export class AuthModule { }
+export class AuthModule {
+}
--- src/app/feature/auth/pages/login/login.component.ts
+++ src/app/feature/auth/pages/login/login.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AppBaseComponent} from "../../../../core/utils/AppBaseComponent";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule,
+    NgClass
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent extends AppBaseComponent {
+
+  public loginForm: FormGroup;
+
+  constructor(private router: Router,
+              private fb: FormBuilder) {
+
+    super();
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required , Validators.minLength(6)]]
+    });
+
+  }
+
+  public signIn(): void {
+    if (this.loginForm.valid) {
+      alert("todo ok")
+
+    } else {
+      alert("Error en Formulario");
+      //borrar en despliegue
+      console.log(this.getAllErrors(this.loginForm));
+    }
+
+
+  };
+
+    public userRecord(): void {
+      this.router.navigateByUrl("authentication/record")
+    }
+
+
+  public getErrorForm(field: string): string {
+    let message;
+    if (this.isTouchedField(this.loginForm, field)) {
+      if(this.loginForm.get(field).hasError('required')) {
+            message = "El campo es requerido";
+      }else if(this.loginForm.get(field).hasError('email')) {
+        message = "El formato del mail no es correcto";
+      }
+
+    }
+    return message;
+
+  }
 
 }
